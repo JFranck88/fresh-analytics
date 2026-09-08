3f7014e35083af3c317bba4768e0f0ad63ead697
Francisco <jgalindoj@miumg.edu.gt>
2026-09-08 16:47:15 -0600

    Fix critico: CSRF_TRUSTED_ORIGINS para permitir formularios POST en produccion (HTTPS)

--- fresh_analytics_project/settings.py
+++ fresh_analytics_project/settings.py
@@ -28,6 +28,10 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-solo-para-desarrollo-
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
+CSRF_TRUSTED_ORIGINS = config(
+    "CSRF_TRUSTED_ORIGINS",
+    default="https://fresh-analytics.onrender.com"
+).split(",")
 
 
 # Application definition
